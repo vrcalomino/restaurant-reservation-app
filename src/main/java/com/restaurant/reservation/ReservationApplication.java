c19b6c08f804af6f720e737f93baf179428f2fcf
vrcalomino <vrcalomino@gmail.com>
2024-01-12 18:15:57 -0300

    feat: environment variables with dotenv

--- src/main/java/com/restaurant/reservation/ReservationApplication.java
+++ src/main/java/com/restaurant/reservation/ReservationApplication.java
@@ -1,5 +1,6 @@
 package com.restaurant.reservation;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ReservationApplication {
 
 	public static void main(String[] args) {
+
+		loadSecrets();
 		SpringApplication.run(ReservationApplication.class, args);
 	}
 
+	private static void loadSecrets() {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+	}
 }
